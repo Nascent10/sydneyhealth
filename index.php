3cbafbadec17996eefa0df6867a4b09217eaa4b2
surajbashyal04 <suraj.me12345@gmail.com>
2022-05-19 17:43:37 +1000

    3 changes

--- index.php
+++ index.php
@@ -1,8 +1,24 @@
-<html>
-    <head>
-        <title>Sydney Health Solutions</title>
-    </head>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navigation Menu Demo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="styles.css">
+</head>
 <body>
-    This is home page.
+    <header>
+        <a href="#" class="logo">SAMEER SAINI</a>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Projects</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#" class="cta">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
 </body>
 </html>
