abb549ae953bad7c8381ea26617112c24d874e91
surajbashyal04 <suraj.me12345@gmail.com>
2022-05-20 10:34:30 +1000

    original

---
+++ about.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navigation Menu Demo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header>
+        <img src="siteimages/logo.png">
+        <nav>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="appointment.html">Appointment</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="#" class="cta">Sign In</a></li>
+                <li><a href="#" class="cta">Register</a></li>
+            </ul>
+        </nav>
+    </header>
+<img class="wehelp" src="siteimages/wehelp.png">
+<ul class="afterwehelp">
+    <li><a href="#">Explore</a></li>
+    <li><a href="#">Make an appointment now</a></li>
+    <li><a href="#">Find a location</a></li>
+</ul>
+<div class="box">
+    <section class="item1">
+      <img class="iwantto" src="siteimages/homepage-iwantto.png">
+    </section>
+    <aside class="item2"> 
+        <p>At Sydney Health Solutions, we are committed to supporting practitioners by delivering timely, accurate and high quality diagnostic results. We know that patients all across NSW rely on us to collect, analyse and report results, and it is a responsibility we take very seriously.</p>
+    </aside>
+</div>
+<div="afteriwanto"><p>How we make it easy?</P></div>
+</body>
+</html>
--- index.php
+++ index.php
@@ -9,16 +9,35 @@
 </head>
 <body>
     <header>
-        <a href="#" class="logo">SAMEER SAINI</a>
+        <img src="siteimages/logo.png">
         <nav>
             <ul>
-                <li><a href="#">Home</a></li>
-                <li><a href="#">About Us</a></li>
-                <li><a href="#">Projects</a></li>
-                <li><a href="#">Blog</a></li>
-                <li><a href="#" class="cta">Contact</a></li>
+                <li><a href="index.php">Home</a></li>
+                <li><a href="appointment.php">Appointment</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="about.php">About Us</a></li>
+                <li><a href="#" class="cta">Sign In</a></li>
+                <li><a href="#" class="cta">Register</a></li>
             </ul>
         </nav>
     </header>
+<img class="wehelp" src="siteimages/wehelp.png">
+<ul class="afterwehelp">
+    <li><a href="#">Explore</a></li>
+    <li><a href="#">Make an appointment now</a></li>
+    <li><a href="#">Find a location</a></li>
+</ul>
+<div><section>
+      <img class="iwantto" src="siteimages/homepage-iwantto.png">
+</section>
+     <aside> <p>At Sydney Health Solutions, we are committed to supporting practitioners by delivering timely, accurate and high quality diagnostic results. We know that patients all across NSW rely on us to collect, analyse and report results, and it is a responsibility we take very seriously.</p>
+</aside>
+</div>
+<div="afteriwanto"><p>How we make it easy?</P></div>
+<br>
+<br>
+<br>
+<br>
+dsf
 </body>
 </html>
